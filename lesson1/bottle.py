08d14e609aa7f4f84faef1a0be56d1165c05eca9
George Astakhov <goha1115@mail.ru>
2023-07-04 21:06:45 +0300

    changes in lesson1

--- lesson1/bottle.py
+++ lesson1/bottle.py
@@ -15,13 +15,14 @@
 class Bottle:
 
     def __init__(self, color, volume):
-        ...
+        self.color = str(color)
+        self.volume = float(volume)
 
 
 bottle_1 = Bottle("Красная", 0.7)
-bottle_2 = ...
-bottle_3 = ...
+bottle_2 = Bottle("Белая", 0.3)
+bottle_3 = Bottle("Черная", 1.0)
 
-print(bottle_1.color, bottle_1.value)  # Красная 0.7
-print(bottle_2.color, bottle_2.value)  # Белая 0.3
-print(bottle_3.color, bottle_3.value)  # Черная 1.0
+print(bottle_1.color, bottle_1.volume)  # Красная 0.7
+print(bottle_2.color, bottle_2.volume)  # Белая 0.3
+print(bottle_3.color, bottle_3.volume)  # Черная 1.0
