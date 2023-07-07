2f88768485217b8f43deccd38e2859228f171a89
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-07-07 13:26:47 +0300

    Update bottle.py

--- lesson1/bottle.py
+++ lesson1/bottle.py
@@ -8,7 +8,7 @@
 
 - Красную 0.7
 - Белую 0.3
-- Черную 1,0
+- Черную 1.0
 """
 
 
@@ -22,6 +22,6 @@ bottle_1 = Bottle("Красная", 0.7)
 bottle_2 = ...
 bottle_3 = ...
 
-print(bottle_1.color, bottle_1.value)  # Красная 0.7
-print(bottle_2.color, bottle_2.value)  # Белая 0.3
-print(bottle_3.color, bottle_3.value)  # Черная 1.0
+print(bottle_1.color, bottle_1.volume)  # Красная 0.7
+print(bottle_2.color, bottle_2.volume)  # Белая 0.3
+print(bottle_3.color, bottle_3.volume)  # Черная 1.0
